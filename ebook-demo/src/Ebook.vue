9c67478d3527fc6f7e1a105a0c15c3c390557f1d
Monster <monster.j.zhang@mail.foxconn.com>
2018-10-13 22:35:42 +0800

    Initial commit

---
+++ ebook-demo/src/Ebook.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="ebook">
+        <title-bar
+            :ifTitleAndMenuShow="ifTitleAndMenuShow"
+        ></title-bar>
+        <div class="read-wrapper">
+            <div id="read"></div>
+            <div class="mask">
+                <div class="left" @click="prevPage"></div>
+                <div class="center" @click="toggleTitleAndMenu"></div>
+                <div class="right" @click="nextPage"></div>
+            </div>
+        </div>
+        <menu-bar
+            ref="menuBar"
+            :ifTitleAndMenuShow="ifTitleAndMenuShow"
+        ></menu-bar>
+    </div>
+</template>
+<script>
+import Epub from 'epubjs'
+import TitleBar from '@/components/TitleBar.vue'
+import MenuBar from '@/components/MenuBar.vue'
+const DOWNLOAD_URL = '/static/2018_Book_AgileProcessesInSoftwareEngine.epub'
+global.ePub = Epub
+/* eslint-disable space-before-function-paren */
+export default {
+    components: {
+        TitleBar, MenuBar
+    },
+    data() {
+        return {
+            ifTitleAndMenuShow: false
+        }
+    },
+    methods: {
+        toggleTitleAndMenu() {
+            this.ifTitleAndMenuShow = !this.ifTitleAndMenuShow
+            if (!this.ifTitleAndMenuShow) {
+                this.$refs.menuBar.hideSetting()
+            }
+        },
+        prevPage() {
+            // Rendition.prev
+            if (this.rendition) {
+                this.rendition.prev()
+            }
+        },
+        nextPage() {
+            // Rendition.next
+            if (this.rendition) {
+                this.rendition.next()
+            }
+        },
+        // 电子书的解析和渲染
+        showEpub() {
+            // 生成Book
+            this.book = new Epub(DOWNLOAD_URL)
+            console.log(this.book)
+            // 生成Rendition , 通过Book.renderTo
+            this.rendition = this.book.renderTo('read', {
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+            // 通过Rendition.display 渲染电子书
+            this.rendition.display()
+        }
+    },
+    mounted () {
+        this.showEpub()
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import 'assets/styles/global';
+.ebook {
+    position: relative;
+    .read-wrapper {
+        .mask {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 100;
+            display: flex;
+            width: 100%;
+            height: 100%;
+            .left {
+                flex: 0 0 px2rem(100);
+            }
+            .center {
+                flex: 1;
+            }
+            .right {
+                flex: 0 0 px2rem(100);
+            }
+        }
+    }
+}
+</style>
---
+++ ebook-demo/src/components/MenuBar.vue
@@ -0,0 +1,88 @@
+<template>
+<div class="menu-bar">
+    <transition name="slide-up">
+        <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
+            <div class="icon-wrapper">
+                <span class="icon icon-menu"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon icon-progress"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon icon-bright"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon icon-a" @click="showSetting">A</span>
+            </div>
+        </div>
+    </transition>
+    <transition name="slide-up">
+        <div class="setting-wrapper" v-show="ifSettingShow">
+            <div class="setting-font-size">
+
+            </div>
+        </div>
+    </transition>
+</div>
+</template>
+<script>
+export default {
+    props: {
+        ifTitleAndMenuShow: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            ifSettingShow: false
+        }
+    },
+    methods: {
+        showSetting() {
+            this.ifSettingShow = true
+        },
+        hideSetting() {
+            this.ifSettingShow = false
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/styles/global';
+.menu-bar {
+    .menu-wrapper {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 101;
+        display: flex;
+        width: 100%;
+        height: px2rem(48);
+        background: white;
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+        .icon-wrapper {
+            flex: 1;
+            @include center;
+            .icon-progress {
+                font-size: px2rem(28);
+            }
+            .icon-bright {
+                font-size: px2rem(24);
+            }
+        }
+    }
+    .setting-wrapper {
+        position: absolute;
+        bottom: px2rem(48);
+        left: 0;
+        width: 100%;
+        height: px2rem(60);
+        background: white;
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, .15);
+        .setting-font-size {
+
+        }
+    }
+}
+</style>
